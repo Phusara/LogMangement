c035a4a911fd11a6da179ef40d94b1988561c339
Phusara <pozzuoli97@gmail.com>
2025-11-12 18:19:29 +0700

    .env.example

--- db/02_init.sql
+++ db/02_init.sql
@@ -2,8 +2,10 @@ DROP INDEX IF EXISTS idx_tenant;
 DROP INDEX IF EXISTS idx_timestamp;
 DROP INDEX IF EXISTS idx_event_type;
 DROP INDEX IF EXISTS idx_ip;
+DROP INDEX IF EXISTS idx_logs_tenant_id;
 
 CREATE INDEX idx_timestamp ON logs (timestamp);
 CREATE INDEX idx_tenant ON tenants (tenant_id);
 CREATE INDEX idx_event_type ON logs (event_type);
 CREATE INDEX idx_ip ON logs (src_ip);
+CREATE INDEX idx_logs_tenant_id ON logs (tenant_id);
